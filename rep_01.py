f75a13349212455cedbd9fce754b53b0b921d868
Guilherme Carvalho <gui.ifsp11@gmail.com>
2014-08-27 00:05:59 -0300

    Exemplo de iteração com o comando for

---
+++ rep_01.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+# usando uma lista de números inteiros usando a função range
+for i in range(10):
+    print '=> %d' % i
+
+# usando uma lista com objetos string
+lista = ['ola mundo', 'my first program']
+for text in lista:
+    print '=> %s' % text
+
+# usando uma variável do tipo string
+for letter in 'Guilherme':
+    print '=> %s' % letter
