7dfa20f05bff76256e6adbb9274d7d312b5833c8
Guilherme Carvalho <gui.ifsp11@gmail.com>
2014-08-27 20:38:57 -0300

    Exemplo de programação funciona utilizando o zip

---
+++ funcional_zip.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+# naípes das cartas
+naipes = ['E', 'O', 'C', 'P'] * 13
+
+# cria as 52 cartas do baralho
+cartas = range(1, 14) * 4
+
+# vincula cada carta a um naípe
+baralho = zip(naipes, cartas)
+
+# organiza as cartas do baralho
+baralho.sort()
+
+print baralho
